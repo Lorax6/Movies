9edfc6026a1e4ef981f6348818dd6e38f28b53fb
Lorax6 <tiasajeev6@gmail.com>
2025-08-19 11:47:13 -0700

    Added browser routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
 
 import './App.css'
 import MovieCard from './components/MovieCard'
+import Favorites from './pages/Favorites';
 import Home from './pages/Home';
+import {Routes,Route} from "react-router-dom"
 
 function App() {
 
   const movieNumber = 3;
   return (
     <> 
-      <Home/>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Favorites/>}/>
+        </Routes>
+      </main>
     </>
      
-  )
+  );
 }
 
-function Text({text}){
-  return(
-    <div>
-      <p>{text}</p>
-    </div>
-      
-  )
-}
 export default App
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+function Favorites(){
+    return  <div className="favorites-empty">
+        <h2>No Favroite movies yet</h2>
+        <p>Start adding movies o your favorites and they will appear here</p>
+    </div>
+
+}
+
+export default Favorites;
